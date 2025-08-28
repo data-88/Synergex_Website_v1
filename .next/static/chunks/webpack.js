8fc99ba3634281582976f7a58599f208d47522d1
InfiniteNow <23291910+data-88@users.noreply.github.com>
2025-08-28 12:32:44 +0200

    cahnges 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cab352965e410ea3"; }
+/******/ 		__webpack_require__.h = function() { return "966298dd15fb8b1b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
